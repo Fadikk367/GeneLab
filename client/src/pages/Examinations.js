63ed1a08a640471d5547c00b534ad39973f0e7f6
Adrian Furman <fadikk367@gmail.com>
2020-12-21 16:30:26 +0100

    Make use of testFamilies in mock Page components

--- client/src/pages/Examinations.js
+++ client/src/pages/Examinations.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { getAllTestFamilies, deleteTestFamily } from 'state/actions/testFamilies.actions';
+
 
 const Examinations = () => {
+  const testFamilies = useSelector(state => state.testFamilies.familyList)
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(getAllTestFamilies());
+  }, [dispatch]);
+
+  const renderedTests = testFamilies.map(test => (
+    <li key={test.id}>
+      {test.id} - {test.familyname}
+      <button onClick={() => dispatch(deleteTestFamily(test.id))}>x</button>
+    </li>
+  ));
+
   return (
     <div>
       Strona z dostepnymi badaniami
+      <ul>
+        {renderedTests}
+      </ul>
     </div>
   )
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { createTestFamily } from 'state/actions/testFamilies.actions';
+
 
 const Home = () => {
+  const [familyName, setFamilyName] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleAddDiagnosticTestsFamily = async e => {
+    e.preventDefault();
+
+    if (familyName) {
+      setIsLoading(true);
+      dispatch(createTestFamily(familyName))
+        .then(() => setIsLoading(false))
+        .catch(() => setIsLoading(false));
+    }
+  }
   return (
     <div>
       Home page
+
+      <form onSubmit={handleAddDiagnosticTestsFamily}>
+        <label>Family name:</label><br />
+        <input type="text" name="familyName" value={familyName} onChange={e => setFamilyName(e.target.value)}/>
+        <button type='submit'>dodaj</button>
+        <span>{isLoading ? 'loading...' : null}</span>
+      </form>
     </div>
   )
 }
