02ffb0a07cbd0f93df06e5fea596f5f5b51271d0
Adrian Furman <fadikk367@gmail.com>
2020-12-25 19:34:38 +0100

    Create redux state logic for biological materials

--- client/src/state/rootReducer.js
+++ client/src/state/rootReducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 
 import testCategoryReducer from './testCategory/testCategoryReducer';
+import biologicalMaterialReducer from './biologicalMaterial/biologicalMaterialReducer';
 
 
 const rootReducer = combineReducers({
-  testCategory: testCategoryReducer
+  testCategory: testCategoryReducer,
+  biologicalMaterial: biologicalMaterialReducer,
 });
 
 export default rootReducer;
