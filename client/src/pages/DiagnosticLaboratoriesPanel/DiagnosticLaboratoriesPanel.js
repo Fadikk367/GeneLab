ef3247a0e5dc7493f2db290ee36dcd6d4e518b58
Adrian Furman <fadikk367@gmail.com>
2020-12-27 23:25:10 +0100

    Add empty panel for diagnostic laboratories

---
+++ client/src/pages/DiagnosticLaboratoriesPanel/DiagnosticLaboratoriesPanel.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AddDiagnosticLaboratoryForm } from './components';
+
+const DiagnosticLaboratoriesPanel = () => {
+  return (
+    <div>
+      DiagnosticLaboratoriesPanel<br />
+      <AddDiagnosticLaboratoryForm />
+    </div>
+  )
+}
+
+export default DiagnosticLaboratoriesPanel
---
+++ client/src/pages/DiagnosticLaboratoriesPanel/components/AddDiagnosticLaboratoryForm/AddDiagnosticLaboratoryForm.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddDiagnosticLaboratoryForm = () => {
+  return (
+    <div>
+      AddDiagnosticLaboratoryForm
+    </div>
+  )
+}
+
+export default AddDiagnosticLaboratoryForm;
