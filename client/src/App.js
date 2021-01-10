4c2d5d3f455b46932765d894eb7c560aec40efe5
Adrian Furman <fadikk367@gmail.com>
2021-01-10 10:53:37 +0100

    Make use of protected routes and segregate nav links that they depend on auth state now

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import { Home, TestCatalog, About, Cart, OnlineResults, AdministrationPanel, NotFound, DiagnosticLaboratories, Login } from './pages';
 
-import { BasketPreview } from 'common/components';
+import { BasketPreview, ProtectedRoute } from 'common/components';
 import { Layout, Sidebar, Center, Page, Header, Footer } from './Layout.css';
 import GlobalStyles from'./index.css.js';
 
 
 const App = () => {
+  const auth = useSelector(state => state.auth);
+
+  const publicLinks = [
+    <li><Link to='/'>Strona główna</Link></li>,
+    <li><Link to='/test-catalog'>Katalog Badań</Link></li>,
+    <li><Link to='/results'>Wyniki online</Link></li>,
+    <li><Link to='/about'>O nas</Link></li>
+  ]
+  const privateLinks = [
+    <li><Link to='/admin-panel'>Panel Administracyjny</Link></li>,
+    <li><Link to='/laboratories'>Pracownie</Link></li>,
+  ]
+
+  const navLinks = auth.isAuthentificated ? [...publicLinks, ...privateLinks] : publicLinks;
+
   return (
     <Layout>
       <GlobalStyles />
@@ -16,12 +32,7 @@ const App = () => {
         <h1>GeneLab</h1>
         <nav>
           <ul>
-            <li><Link to='/'>Strona główna</Link></li>
-            <li><Link to='/test-catalog'>Katalog Badań</Link></li>
-            <li><Link to='/results'>Wyniki online</Link></li>
-            <li><Link to='/admin-panel'>Panel Administracyjny</Link></li>
-            <li><Link to='/laboratories'>Pracownie</Link></li>
-            <li><Link to='/about'>O nas</Link></li>
+            {navLinks}
           </ul>
         </nav>
       </Sidebar>
@@ -35,8 +46,8 @@ const App = () => {
             <Route path='/' exact component={Home} />
             <Route path='/test-catalog' exact component={TestCatalog} />
             <Route path='/results' exact component={OnlineResults} />
-            <Route path='/laboratories' component={DiagnosticLaboratories} />
-            <Route path='/admin-panel' component={AdministrationPanel} />
+            <ProtectedRoute path='/laboratories' auth={auth} component={DiagnosticLaboratories} />
+            <ProtectedRoute path='/admin-panel' auth={auth} component={AdministrationPanel} />
             <Route path='/login' component={Login} />
             <Route path='/cart' component={Cart} />
             <Route path='/about' component={About} />
