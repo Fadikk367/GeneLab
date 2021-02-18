acfd7ba6e2f98e32058ff9c013ddd05d53381b3b
Adrian Furman <fadikk367@gmail.com>
2021-02-18 21:23:09 +0100

    Style Cart component in Order page

--- client/src/pages/Order/components/Cart/Cart.js
+++ client/src/pages/Order/components/Cart/Cart.js
@@ -2,6 +2,13 @@ import React, { useEffect } from 'react';
 import { useFormContext } from 'react-hook-form';
 import { useSelector, useDispatch } from 'react-redux';
 
+import List from '@material-ui/core/List';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import IconButton from '@material-ui/core/IconButton';
+import DeleteIcon from '@material-ui/icons/Delete';
+
+import { ListTitle, ListItem, ListItemText } from './Cart.css';
+
 import { removeProductFromBasket } from 'state/clientBasket/clientBasketActions';
 
 
@@ -12,28 +19,45 @@ const Cart = () => {
 
   useEffect(() => {
     register({ name: 'products', type: 'custom' }, { validate: items => items.length !== 0 });
-    setValue('products', examinationsInCart)
+    setValue('products', examinationsInCart);
   }, [examinationsInCart]);
 
   return (
-    <div>
-      {examinationsInCart.length !== 0 
-      ? (
-        <>
-          <h3>Twoje badania:</h3>
-          <ul>
-            {examinationsInCart.map(item => (
-              <li key={item.id}>
-                {item.name}, {item.price}
-                <button onClick={() => dispatch(removeProductFromBasket(item.id))}>&times;</button>
-              </li>
-            ))}
-          </ul>
-        </>
-      ) : (
-        <h3>Koszyk jest pusty, wybierz badania dostepne w ofercie</h3>
-      )}
-    </div>
+    <>
+      <ListTitle variant='h6'>
+        Twoje badania: ({examinationsInCart.length})
+      </ListTitle>
+      <List>
+        {examinationsInCart.map(item => (
+          <ListItem key={item.id} divider>
+            <ListItemText flex={5}>{item.name}</ListItemText>
+            <ListItemText flex={1}>{item.price} PLN</ListItemText>
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="delete">
+                <DeleteIcon onClick={() => dispatch(removeProductFromBasket(item.id))}/>
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        ))}
+        {examinationsInCart.length === 0 ? (
+          <ListItem>
+            <ListItemText>Koszyk jest pusty, aby kontynuować, wybierz badania z dostępnej oferty</ListItemText>
+          </ListItem>
+        ) : (
+          <ListItem key={-1} divider>
+            <ListItemText flex={5} textAlign='right'>Suma:</ListItemText>
+            <ListItemText flex={1}>
+              {examinationsInCart.reduce((total, item) => total += parseFloat(item.price), 0.0).toFixed(2)} PLN
+            </ListItemText>
+            <ListItemSecondaryAction>
+              <IconButton edge="end" aria-label="delete" disabled style={{ visibility: 'hidden' }}>
+                <DeleteIcon/>
+              </IconButton>
+            </ListItemSecondaryAction>
+          </ListItem>
+        )}
+      </List>
+    </>
   )
 }
 
