be742b46f1cd931ff10f93abe7d37e984a5f0ecb
Adrian Furman <fadikk367@gmail.com>
2021-03-04 23:55:50 +0100

    Add final step to Order multistep form to display placed order confirmation and access code

--- client/src/pages/Order/components/index.js
+++ client/src/pages/Order/components/index.js
@@ -1,5 +1,6 @@
 export { default as Cart } from './Cart';
 export { default as PersonalDataForm } from './PersonalDataForm';
 export { default as LocationForm } from './LocationForm';
-export { default as OrderSummary } from './OrderSummary';
 export { default as PaymentMethodForm } from './PaymentMethodForm';
+export { default as OrderSummary } from './OrderSummary';
+export { default as OrderConfirmation } from './OrderConfirmation';
