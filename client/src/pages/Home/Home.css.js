537656cff711a197fedb9be779dc31cd9e33234c
Adrian Furman <fadikk367@gmail.com>
2021-03-07 00:00:25 +0100

    Create basic content for Home page

--- client/src/pages/Home/Home.css.js
+++ client/src/pages/Home/Home.css.js
@@ -1,1 +1,36 @@
 import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: calc(100vh - 60px);
+  background-image: url('photo1.jpg');
+  background-image: center center;
+  background-size: cover;
+  background-repeat: no-repeat;
+  position: relative;
+`;
+
+
+export const Header = styled.header`
+  padding: 60px;
+  background-color: rgba(233,233,233, 0.7);
+  position: absolute;
+  top: 50%;
+  width: 100%;
+  transform: translateY(-50%);
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+export const Offers = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 30px;
+
+`;
+
+export const OfferCard = styled.article`
+  background-color: grey;
+  /* width: 300px; */
+  height: 400px;
+`;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,15 +1,21 @@
-import React from 'react'
+import React from 'react';
+
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+import { Wrapper, Header, Offers, OfferCard } from './Home.css';
+
+
 
 const Home = () => {
   return (
-    <div>
-      <h1>Welcome to GeneLab</h1>
-      <hr/>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam sequi cumque illo sunt quas, voluptates non eos ducimus fugiat odit minus laboriosam, velit dolorem quo suscipit perspiciatis quia dolorum possimus.</p>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quasi nesciunt totam ut amet maxime iure dolore beatae magni corrupti!</p>
-      <h2>Section1</h2>
-      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, illum eveniet enim nulla deleniti, deserunt facere accusantium, mollitia quas quos repellendus tempora officiis laudantium? Ab adipisci earum, id voluptas ratione, illum explicabo minus voluptate harum sequi sed? Magnam, quos ea. Molestiae aut, eaque laborum fugiat, nostrum ab similique qui accusamus quam provident autem doloribus praesentium aliquid, illo eius nobis obcaecati error libero asperiores facere rerum impedit alias sint a! Ut, eveniet. Accusamus ipsam velit placeat sapiente fugit voluptatibus sint neque inventore repellendus quasi, iusto harum molestias mollitia quaerat saepe? Ea, soluta delectus aut impedit enim corporis alias exercitationem earum sint.</p>
-    </div>
+    <Wrapper>
+      <Header>
+        <Typography variant='h3' align='center'>Witaj w GeneLab</Typography>
+        <Typography variant='h4' align='center'>Zadbaj o swoje zdrowie już teraz</Typography>
+      </Header>
+    </Wrapper>
   )
 }
 
