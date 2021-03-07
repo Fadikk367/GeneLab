9e701159ad64e671739ce882c2db40169d87123a
Adrian Furman <fadikk367@gmail.com>
2021-03-07 23:58:37 +0100

    Migrate to material-ui table for pending examinations list

--- client/src/pages/DiagnosticLaboratory/components/PendingExaminationsList/PendingExaminationsList.css.js
+++ client/src/pages/DiagnosticLaboratory/components/PendingExaminationsList/PendingExaminationsList.css.js
@@ -1,37 +1,15 @@
 import styled from 'styled-components';
+import Cell from '@material-ui/core/TableCell';
 
 
-export const Table = styled.table`
-  width: 1000px;
+export const TableContainer = styled.table`
   padding: 30px;
+  width: 100%;
   font-size: 1.1em;
 `;
 
-export const TableBody = styled.tbody`
-
-`;
-
-export const TableCell = styled.td`
-  flex: ${props => props.flex || 'none'};
-  width: ${props => props.width || 'default'};
-  text-align: ${props => props.align || 'left'};
-  padding: 12px;
-`;
-
-export const TableHead = styled.thead`
-  background-color: #2fad98;;
-`;
-
-export const TableRow = styled.tr`
-  display: flex;
-  justify-content: space-between;
-  border-bottom: 1px solid #AAAAAA;
-
-  tbody &:nth-child(odd) {
-    background-color: #f7f5f5;
-  }
-  
-  tbody &:hover {
-    background-color: #cbf7f1;;
+export const TableCell = styled(Cell)`
+  && {
+    font-size: 1em;
   }
 `;
--- client/src/pages/DiagnosticLaboratory/components/PendingExaminationsList/PendingExaminationsList.js
+++ client/src/pages/DiagnosticLaboratory/components/PendingExaminationsList/PendingExaminationsList.js
@@ -1,40 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-import { Table, TableBody, TableHead, TableRow, TableCell } from './PendingExaminationsList.css';
+import { TableContainer, TableCell } from './PendingExaminationsList.css';
+
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+import CreateIcon from '@material-ui/icons/Create';
+import Button from '@material-ui/core/Button';
 
 
 const PendingExaminationsList = ({ pendingExaminations = [] }) => {
   return (
-    <Table>
-      <TableHead>
-        <TableRow>
-          <TableCell width={30}>Lp.</TableCell>
-          <TableCell flex={4}>Nazwa</TableCell>
-          <TableCell flex={2} align='right'>Przedział</TableCell>
-          <TableCell flex={2}>Jednostka</TableCell>
-          <TableCell flex={2}>Materiał</TableCell>
-          <TableCell flex={1}>id</TableCell>
-          <TableCell flex={1}></TableCell>
-        </TableRow>
-      </TableHead>
-      <TableBody>
-        {pendingExaminations.map((row, i) => (
-          <TableRow key={row.name}>
-            <TableCell width={30}>{i}</TableCell>
-            <TableCell flex={4}>{row.name}</TableCell>
-            <TableCell flex={2} align='right'>{row.min}-{row.max}</TableCell>
-            <TableCell flex={2}>{row.unit}</TableCell>
-            <TableCell flex={2}>{row.material}</TableCell>
-            <TableCell flex={1}>{row.id}</TableCell>
-            <TableCell flex={1} align='right'>
-              <Link to={`/laboratory/examinations/${row.id}/do`}>Wykonaj</Link>
-            </TableCell>
+    <TableContainer>
+      <Table dense>
+        <TableHead style={{ backgroundColor: '#2fad98' }}>
+          <TableRow>
+            <TableCell>Lp.</TableCell>
+            <TableCell>Nazwa</TableCell>
+            <TableCell align='right'>Przedział</TableCell>
+            <TableCell>Jednostka</TableCell>
+            <TableCell>Materiał</TableCell>
+            <TableCell>id</TableCell>
+            <TableCell align='right'></TableCell>
           </TableRow>
-        ))}
-      </TableBody>
-      {pendingExaminations.length === 0 ? 'Brak oczekujących badań...' : null}
-    </Table>
+        </TableHead>
+        <TableBody>
+          {pendingExaminations.map((row, i) => (
+            <TableRow key={row.name} hover>
+              <TableCell>{i}</TableCell>
+              <TableCell>{row.name}</TableCell>
+              <TableCell align='right'>{row.min}-{row.max}</TableCell>
+              <TableCell>{row.unit}</TableCell>
+              <TableCell>{row.material}</TableCell>
+              <TableCell>{row.id}</TableCell>
+              <TableCell align='right'>
+                <Link to={`/laboratory/examinations/${row.id}/do`} style={{ textDecoration: 'none' }}>
+                  <Button endIcon={<CreateIcon />}>
+                    wykonaj
+                  </Button>
+                </Link>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+        {pendingExaminations.length === 0 ? 'Brak oczekujących badań...' : null}
+      </Table>
+    </TableContainer>
   )
 }
 
